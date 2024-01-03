5c9aff10f6ee05d8bbea73962c3e3e8a9052606e
Matija Lojovic <matija.lojovic@yahoo.com>
2024-01-03 20:01:44 +0100

    Refactored clang-tidy script to work without CMakeLists

--- clang-tidy/clang-tidy.sh
+++ clang-tidy/clang-tidy.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+mkdir ../visual/build && cd ../visual/build
+qmake -makefile ../GraphVisual/graph.pro > /dev/null
+bear -- make > /dev/null
+cd ../../clang-tidy
+
 CURRENT_DIR=$(pwd)
 PROJECT_DIR="${CURRENT_DIR}/../visual/GraphVisual"
-BUILD_DIR="${PROJECT_DIR}/../build-graph-Desktop_Qt_5_15_2_GCC_64bit-Debug"
-
-
-cd "${BUILD_DIR}" || exit
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON "${PROJECT_DIR}"
-
+BUILD_DIR="${PROJECT_DIR}/../build"
+OUTPUT_FILE="${CURRENT_DIR}/clang-tidy-output.txt"
 
 find "${PROJECT_DIR}" \( -name "*.cpp" -o -name "*.h" \) -print0 \
-  | xargs -0 clang-tidy \
+  | xargs -0 -n 1 clang-tidy \
       -p "${BUILD_DIR}" \
       --header-filter=".*" \
       -extra-arg=-isystem,/home/matija/Qt/5.15.2/gcc_64/include\
@@ -18,5 +19,8 @@ find "${PROJECT_DIR}" \( -name "*.cpp" -o -name "*.h" \) -print0 \
       -extra-arg=-I/usr/include/c++/11 \
       -extra-arg=-I/usr/include/x86_64-linux-gnu/c++/11 \
       -extra-arg=-I"${BUILD_DIR}"\
-      -quiet
+      -checks=-clang-analyzer-webkit.RefCntblBaseVirtualDtor \
+  > "${OUTPUT_FILE}"
+
+rm -rf ../visual/build
 
